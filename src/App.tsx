a92dbc56ac3a05c13f69d7b3a1cf9f78d29f9a14
Rystem <2starcraft@list.ru>
2023-02-26 19:33:13 +0300

    deploy fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { ThemeProvider } from './context/ThemeContext';
 import { AppRoutes } from './routes/Routes';
 import './assets/sass/App.scss';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <ThemeProvider>
         <AppRoutes />
       </ThemeProvider>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
