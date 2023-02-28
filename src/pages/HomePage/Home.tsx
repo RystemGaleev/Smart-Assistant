17a734f40102e68a26a9210e223e81520417034e
Rystem <2starcraft@list.ru>
2023-03-01 00:50:35 +0300

    clear

--- src/pages/HomePage/Home.tsx
+++ src/pages/HomePage/Home.tsx
@@ -37,32 +37,3 @@ export const Home = () => {
     </Layout>
   );
 };
-{
-  /* <div className="main__column">
-                <h3 className="main__subtitle">Tast Manager</h3>
-                <div className="main__text">
-                  The Task Manager is an ideal solution for those who want to stay organized and monitor the progress of their
-                  projects. This allows you to easily manage tasks, create cards with specific details, assign names and
-                  descriptions, change the status of existing tasks, delete unnecessary tasks and track progress as they are
-                  completed. You can filter cards by their completion status.
-                </div>
-              </div>
-              <div className="main__column">
-                <h3 className="main__subtitle">Library</h3>
-                <div className="main__text">
-                  The Library allows users to store a variety of useful content, such as messages, information, interesting
-                  topics, useful links and much more. It provides an easy way to manage and organize all the information you want
-                  to keep. Additionally, you can customize the library with tags to help you quickly search by content. You can
-                  filter cards by their completion status and search by their title or description
-                </div>
-              </div>
-              <div className="main__column">
-                <h3 className="main__subtitle">Weather</h3>
-                <div className="main__text">
-                  The Weather provides users with an interactive and easy-to-use interface to get information about the current
-                  weather in any country or city. The weather tab displays detailed information such as temperature, wind speed,
-                  humidity, and precipitation for the current day. The interface is designed to be user-friendly, providing him
-                  with the necessary information quickly and efficiently.
-                </div>
-              </div> */
-}
